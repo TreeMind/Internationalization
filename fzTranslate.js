7e85b70138cc0957bc5c4be2f23b87f7255a9564
Yuvaraj Palanisamy <M1033416@mindtree.com>
2016-02-16 13:40:16 +0530

    Inital commit

---
+++ fzTranslate.js
@@ -0,0 +1,27 @@
+﻿define(['angular', 'pascalprecht.translate','tmh.dynamiclocale'], function (angular) {
+    var fzTranslate = angular.module("fzTranslate", ['pascalprecht.translate', 'tmh.dynamicLocale']).
+    config(['$translateProvider', '$translatePartialLoaderProvider','tmhDynamicLocaleProvider', function ($translateProvider, $translatePartialLoaderProvider,tmhDynamicLocaleProvider ) {
+        /*$translateProvider.useStaticFilesLoader({
+            prefix: "localization_resources/resource-",
+            suffix: '.json'
+        });*/
+        $translateProvider.useLoader('$translatePartialLoader', {
+            urlTemplate: 'Resources/FZ/{lang}/{part}/resource-{lang}.json'
+        });
+        // Tell the module what language to use by default
+        tmhDynamicLocaleProvider.localeLocationPattern('../lib/angular-i18n/angular-locale_{{locale}}.js');
+        $translateProvider.preferredLanguage('en_US');
+
+       // $translateProvider.useLocalStorage();
+    }])
+    .filter('currencys', ['$filter', '$locale',
+    function ($filter, $locale) {
+        return function (num) {
+            debugger
+            var sym = $locale.NUMBER_FORMATS.CURRENCY_SYM;
+            return $filter('currency')(num, '<span>' + sym + '</span>');
+        };
+    }
+    ]);
+    return fzTranslate;
+});
---
+++ fzTranslatorService.js
@@ -0,0 +1,19 @@
+﻿define(['angular', 'fzTranslate', 'pascalprecht.translate', 'angular.translate', 'tmh.dynamiclocale'], function (angular) {
+    'use strict';
+    return angular.module("fzTranslate").factory("fzTranslatorService", function ($q, $translate, $translatePartialLoader, tmhDynamicLocale, $rootScope, $filter) {
+       
+        var fzTranslatorService = {
+            setLanguage: function (cultureString,moduleName) {
+                $translatePartialLoader.addPart(moduleName);
+                //$translate.refresh();
+                tmhDynamicLocale.set(cultureString);
+                $translate.use(cultureString);
+            }
+        };
+        $rootScope.$on('$translateChangeSuccess', function (event, data) {
+            $filter('currencys');
+        });
+        return fzTranslatorService;
+    });
+});
+
